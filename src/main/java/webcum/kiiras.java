4ab2f611a4b1e75ddbb42e1abeb3977ddc32984c
branc <branc@DESKTOP-OBEQ1DT.mshome.net>
2016-10-05 23:55:21 +0200

    alma

--- src/main/java/webcum/kiiras.java
+++ src/main/java/webcum/kiiras.java
@@ -29,6 +29,7 @@ public class kiiras extends javax.swing.JFrame {
         client[0] = new send();
         client[1] = new send();
         client[2] = new send();
+        server = new receive();
     }
     send[] client = new send[3];
     receive server = new receive();
@@ -110,6 +111,19 @@ public class kiiras extends javax.swing.JFrame {
         protected void done() {
         }
 
+    }
+
+    class external_cam_start extends SwingWorker<Void, Void> {
+
+        @Override
+        protected Void doInBackground() throws Exception {
+            server.start();
+            return null;
+        }
+
+        @Override
+        protected void done() {
+        }
     }
 
     class external_cam_receive1 extends SwingWorker<Void, Void> {
@@ -288,6 +302,7 @@ public class kiiras extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         send = !send;
+        new external_cam_start().execute();
         server_setup();
         /*client[0].setup(1, "127.0.0.1", 6666);
         client[1].setup(2, "127.0.0.1", 6666);
@@ -302,7 +317,7 @@ public class kiiras extends javax.swing.JFrame {
     void server_setup() {
         try {
             if (send) {
-                server.start();
+                //server.start();
             } else {
                 server.stop();
             }
