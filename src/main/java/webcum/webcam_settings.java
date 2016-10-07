b69a1fc959f95bfb1c7b63fb9741e286c87bf4ec
branc <branc@DESKTOP-OBEQ1DT.mshome.net>
2016-10-07 19:53:15 +0200

    vga felbontás

--- src/main/java/webcum/webcam_settings.java
+++ src/main/java/webcum/webcam_settings.java
@@ -29,7 +29,9 @@ public class webcam_settings extends javax.swing.JFrame {
     public void kamera_res_setup() {
         if (Cm.webcam.getName().contains("HD") || Cm.webcam.getName().contains("EasyCamera")) {
             Cm.webcam.setCustomViewSizes(new Dimension[]{WebcamResolution.HD720.getSize()});//új felbontás regisztrálása
-            Cm.webcam.setViewSize(WebcamResolution.HD720.getSize());//be állítása
+           // Cm.webcam.setViewSize(WebcamResolution.HD720.getSize());//be állítása HD
+           //VGA
+           Cm.webcam.setViewSize(WebcamResolution.VGA.getSize());//be állítása VGA
         }
     }
 
