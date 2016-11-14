40e10e23a4cbed9f64bef342404708b84b9f2f3c
TWIN-ZEG <TWIN-ZEG@192.168.50.13>
2016-11-14 13:47:36 +0100

    valami 14

--- src/main/java/voice/client/ClientShared.java
+++ src/main/java/voice/client/ClientShared.java
@@ -8,10 +8,10 @@ public class ClientShared {
     // Leaves 5 bytes for the breaker
     public static final int bytesize = 1024 - MultiChatConstants.BREAKER.length(); //1024//*1024 * 8*/;//6144;//18432;//6144;
 
-    public static final float sampleRate = 8000.0F;
-    public static final float frameRate = 8000.0F;
+    public static final float sampleRate = 44100.0F;
+    public static final float frameRate = 44100.0F;
 
-    public static final int sampleSize = 8;
+    public static final int sampleSize = 16;
 
     public ClientShared() {
 
