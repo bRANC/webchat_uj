5f1aa7e358ca241e069961cdc395c55f48862a86
branc <branc@DESKTOP-OBEQ1DT>
2017-04-12 12:33:52 +0200

    fixdmaybe

--- src/main/java/base/client/ChatClient.java
+++ src/main/java/base/client/ChatClient.java
@@ -444,22 +444,22 @@ public class ChatClient implements Runnable, ActionListener {
                 varas(1);
             }//wait for connection
             try {
-                // for (int i = 0; i < SS.size(); i++) {
-                //   if (SS.get(i).name.equals(NickName)) {
-                //     SS.get(i).status = status;
-                // }
-                // }
-                //if (!status_tomb.isEmpty()) {
-                    System.out.println("status: " + status_tomb.size());
-                    //status = status_tomb.get(0);
-                  //  status_tomb.remove(0);
-                    if (status.equals("")) {
-                        System.out.println("set_stat:                " + "SS;" + NickName + ";" + status);
-                        out.write(("SS;" + NickName + ";" + status + MultiChatConstants.BREAKER).getBytes());
-                        out.flush();
-                        status = "";
-                //    }
+                for (int i = 0; i < SS.size(); i++) {
+                    if (SS.get(i).name.equals(NickName)) {
+                        SS.get(i).status = status;
+                    }
                 }
+                //if (!status_tomb.isEmpty()) {
+                System.out.println("status: " + status_tomb.size());
+                //status = status_tomb.get(0);
+                //  status_tomb.remove(0);
+                //if (status.equals("")) {
+                System.out.println("set_stat:                " + "SS;" + NickName + ";" + status);
+                out.write(("SS;" + NickName + ";" + status + MultiChatConstants.BREAKER).getBytes());
+                out.flush();
+                status = "";
+                //    }
+                // }
             } catch (Exception e) {
                 System.out.println("setstat exception: " + e.toString());
             }
